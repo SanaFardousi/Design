3ebd2b92051666fec7df8f1f5439d6c3ba312711
Rassem Alwan <rassemalwan712@gmail.com>
2026-03-13 19:13:31 +0300

    Added Bins to Server

--- MrNadhif_app/MrNadhif-backend/server.js
+++ MrNadhif_app/MrNadhif-backend/server.js
@@ -6,6 +6,7 @@ require('dotenv').config();
 const authRoutes = require('./routes/auth');
 const robotRoutes = require('./routes/robot');
 const reportsRoutes = require('./routes/reports');
+const binsRoutes = require('./routes/bins');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -18,6 +19,7 @@ app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/robot', robotRoutes);
 app.use('/api/reports', reportsRoutes);
+app.use('/api/bins',    binsRoutes); 
 
 // Test route
 app.get('/', (req, res) => {
